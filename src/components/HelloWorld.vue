99e2193d3ef4a9e8a67ab7fe4bed64f8ddea497d
Shadin <buhbuhbabah@gmail.com>
2022-10-23 18:56:28 +0400

    fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,79 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<template lang="pug">
+  div.container
+    div.header
+
+    div.navigation
+
+    div.sidebar
+
+    div.content
+
+    div.clear
+
+    div.footer
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.header {
+  position: absolute;
+  right: 0px;
+  top: 0px;
+  background: #e1f5a4;
+  width: 100%;
+  height: 55px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.container {
+  margin: auto auto;
+  text-align: center;
+  width: 100%;
+  height: 100%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.navigation {
+  position: absolute;
+  top: 55px;
+  left: 0px;
+  background: #edb86d;
+  width: 100%;
+  height: 50px;
 }
-a {
-  color: #42b983;
+
+.sidebar {
+  position: absolute;
+  top: 105px;
+  left: 0px;
+  background: #fae8b9;
+  float: left;
+  width: 17%;
+  height: 100%;
+}
+
+.content {
+  position: absolute;
+  top: 105px;
+  right: 0px;
+  background: #ffffff;
+  float: right;
+  width: 83%;
+  height: 100%;
+}
+.clear {
+  clear: both;
+}
+
+.footer {
+  position: absolute;
+  left: 0px;
+  bottom: 0px;
+  background: #edb86d;
+  width: 100%;
+  height: 50px;
 }
 </style>
