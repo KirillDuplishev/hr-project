5b1791f1408853f78c9c036a484871a1fca428be
Kesha1 <kduplisev@gmail.com>
2022-10-31 16:02:06 +0400

    vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
-<template>
-  <div id="app">
-    <MainPage/>
-  </div>
+<template lang="pug">
+div
+  div(id="app")
+    router-link(to="/MainPage")
+  router-view MainPage
 </template>
 
-<script>
-import MainPage from './views/MainPage.vue'
-// import HelloWorld from './components/HelloWorld.vue'
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: 'App',
-  components: {
-    MainPage
-    // HelloWorld
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
   }
 }
-</script>
-
-<style>
 </style>
